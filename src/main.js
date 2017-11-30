b003ddce6fbbf5a0114694d33d9d1506ae24c667
tara z. manicsic <tzmanics@gmail.com>
2017-11-30 16:31:57 -0500

    include the Kendo ColorPicker

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { KendoColorPicker } from '@progress/kendo-inputs-vue-wrapper'
+import '@progress/kendo-theme-default/dist/all.css'
+import '@progress/kendo-ui'
+
+Vue.component(KendoColorPicker.name, KendoColorPicker)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  components: {
+    KendoColorPicker
+  }
 })
